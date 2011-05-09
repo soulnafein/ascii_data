0572eae95835c5602ac2be628f43432e21bf20cc
David S <soulnafein@gmail.com>
2011-05-10 00:32:01 +0100

    Added float, bool and fixed inheritance problems

--- lib/ascii_data_row.rb
+++ lib/ascii_data_row.rb
@@ -1,17 +1,24 @@
 class AsciiDataRow
+  class << self
+    attr_reader :fields_definitions
+
+    def fields_definitions
+      @fields_definitions ||= {}
+    end
+  end
+
   def self.field(name, range, type)
-    @@fields_definitions ||= {}
-    @@fields_definitions[name] = FieldDefinition.new(type, range)
+    fields_definitions[name] = FieldDefinition.new(type, range)
   end
 
   def self.create_from(ascii_row)
-    AsciiDataRow.new(ascii_row)
+    self.new(ascii_row)
   end
 
   def initialize(ascii_row)
     @fields = {}
     @ascii_row = ascii_row
-    @@fields_definitions.each_pair {|name, definition| @fields[name] = get_value_for_field_definition(definition) }
+    self.class.fields_definitions.each_pair {|name, definition| @fields[name] = get_value_for_field_definition(definition) }
   end
 
   def fields
@@ -26,6 +33,16 @@ class AsciiDataRow
       return nil if text_value.empty?
       return text_value.to_i
     end
+
+    if definition.type == :float
+      return nil if text_value.empty?
+      return text_value.to_f
+    end
+
+    if definition.type == :bool
+      return text_value == '1'
+    end
+
   end
 end
 
--- spec/ascii_data_row_spec.rb
+++ spec/ascii_data_row_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require File.dirname(__FILE__) + '/spec_helper'
 
 describe AsciiDataRow do
   context "when parsing a ascii data row" do
@@ -8,41 +8,71 @@ describe AsciiDataRow do
     #    19-21       3         B     Insurance Group 2
     #    22-31       10     CLHB     Kerb Weight (kg) (Dry Weight for BIKES)
     #    32-41       10     CLHB     Length (mm)
-    #    42-51       10     CLHB     Width (mm)
-    ASCII_LINE = %q{4404201/01/2010 13         1035      3657      1627}
+    #    42-53       10     CLHB     Width (mm)
+    ASCII_LINE = %q{4404201/01/2010 13         1035      3657      1627.23}
 
     it "should load strings in a certain ranges" do
-      class Row < AsciiDataRow
+      class RowA < AsciiDataRow
         field :vehicle_id_number, 1..5, :string
       end
 
-      row = Row.create_from(ASCII_LINE)
+      row = RowA.create_from(ASCII_LINE)
 
       row.fields[:vehicle_id_number].should == "44042"
     end
 
     it "should trim spaces at the beginning and the end of string values" do
-      class Row < AsciiDataRow
+      class RowB < AsciiDataRow
         field :kerb_weight_as_text, 22..31, :string
       end
 
-      row = Row.create_from(ASCII_LINE)
+      row = RowB.create_from(ASCII_LINE)
 
       row.fields[:kerb_weight_as_text].should == "1035"
     end
 
     it "should parse integers correctly" do
-      class Row < AsciiDataRow
+      class RowC < AsciiDataRow
         field :insurance_group_1, 16..18, :int
         field :insurance_group_2, 19..21, :int
         field :kerb_weight, 22..31, :int
       end
 
-      row = Row.create_from(ASCII_LINE)
+      row = RowC.create_from(ASCII_LINE)
 
       row.fields[:insurance_group_1].should == 13
       row.fields[:insurance_group_2].should be_nil
       row.fields[:kerb_weight].should == 1035
+    end
+
+    it "should parse floats correctly" do
+      class RowD < AsciiDataRow
+        field :width, 42..54, :float
+      end
+
+      row = RowD.create_from(ASCII_LINE)
+
+      row.fields[:width].should == 1627.23
+    end
+
+    it "should parse bools correctly" do
+      class RowE < AsciiDataRow
+        field :flag_1, 3..3, :bool
+        field :flag_2, 5..5, :bool
+      end
+
+      row = RowE.create_from("  1 0")
+
+      row.fields[:flag_1].should be_true
+      row.fields[:flag_2].should be_false
+    end
+
+    it "should fucking work" do
+      class B < AsciiDataRow
+        field :field_1, 4..4, :int
+      end
+
+      puts B.fields_definitions
     end
   end
 end
